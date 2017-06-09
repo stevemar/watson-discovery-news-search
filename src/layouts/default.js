751243c439fb950ad080a524299eba59362679da
Ankur Patel <apatel@shutterstock.com>
2017-06-09 14:18:51 -0400

    Fix typo and warnings

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -28,8 +28,8 @@ class DefaultLayout extends React.Component {
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="og:title" content="Watson Discovery New Search" />
-          <meta name="og:description" content={this.props.description} />
+          <meta name="og:title" content="Watson Discovery News Search" />
+          <meta name="og:description" content={this.props.description || 'Search News using Watson Discovery Service'} />
           <link rel="stylesheet" type="text/css" href="/css/watson-react-components.min.css" />
           <link rel="stylesheet" type="text/css" href="/css/application.css"/>
         </head>
@@ -63,7 +63,7 @@ class DefaultLayout extends React.Component {
 }
 
 DefaultLayout.propTypes = {
-  description: PropTypes.string.isRequired,
+  description: PropTypes.string,
   children: PropTypes.node.isRequired,
   initialData: PropTypes.string.isRequired
 };
